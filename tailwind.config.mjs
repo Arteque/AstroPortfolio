403bf7ff8a8c0710fb1726678ce39cc275826a2b
Ahmed Lemssiah <art3que@gmail.com>
2024-05-06 16:25:29 +0200

    Header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors:{
+			light: "#E9E9E9",
+			dark: "#0C254B",
+			blue: "#114595",
+			orange: "#DF6334",
+			green: "#45CB53",
+			lightgreen: "#8FF299"
+		},
+		fontFamily: {
+			'sans': ['Switzer', 'sans-serif'],
+		},
 		extend: {},
 	},
 	plugins: [],
