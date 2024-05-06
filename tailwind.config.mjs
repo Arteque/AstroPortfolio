d69620ba035c58c5642b94ed18e3ee8df2a33de2
Factum <art3que@gmail.com>
2024-05-07 00:05:54 +0200

    hero Section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,5 +15,24 @@ export default {
 		},
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		function({ addBase, theme }) {
+			function extractColorVars(colorObj, colorGroup = '') {
+			  return Object.keys(colorObj).reduce((vars, colorKey) => {
+				const value = colorObj[colorKey];
+	  
+				const newVars =
+				  typeof value === 'string'
+					? { [`--color${colorGroup}-${colorKey}`]: value }
+					: extractColorVars(value, `-${colorKey}`);
+	  
+				return { ...vars, ...newVars };
+			  }, {});
+			}
+	  
+			addBase({
+			  ':root': extractColorVars(theme('colors')),
+			});
+		  },
+	],
 }
