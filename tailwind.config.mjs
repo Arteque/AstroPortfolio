bdf8f53e6a9a3de1131464b4f2e1d5ee65f9eac9
Ahmed Lemssiah <art3que@gmail.com>
2024-05-08 16:15:25 +0200

    Setup

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,12 +3,20 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		colors:{
+			/*
 			light: "#E9E9E9",
 			dark: "#0C254B",
 			blue: "#114595",
 			orange: "#DF6334",
 			green: "#45CB53",
 			lightgreen: "#8FF299"
+			*/
+			text: "rgba(var(--text))",
+			background: "rgba(var(--background))",
+			blue: "rgba(var(blue))",
+			orange: "rgba(var(--orange))",
+			green: "rgba(var(--green))",
+			lightgreen: "rgba(var(--lightgreen))"
 		},
 		fontFamily: {
 			'sans': ['Inter var', 'ui-sans-serif', 'system-ui', 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", 'Segoe UI Symbol', "Noto Color Emoji"],
